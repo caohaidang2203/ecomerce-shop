206f2833192b4f39a42f7065c76bab78f28eb38a
caohaidang2203 <caohaidang2203@gmail.com>
2023-06-09 01:09:00 +0700

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,40 @@
+import React, { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { LoginPage, SignupPage, ActivationPage } from "./Routes.js";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
+import Store from "./redux/store.js";
+import { loadUser } from "./redux/actions/user.js";
+const App = () => {
+  useEffect(() => {
+    Store.dispatch(loadUser());
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/sign-up" element={<SignupPage />} />
+        <Route
+          path="/activation/:activation_token"
+          element={<ActivationPage />}
+        />
+      </Routes>
+      <ToastContainer
+        position="bottom-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+    </BrowserRouter>
+  );
+};
+
+export default App;
